1e6ce860ced0d9af32c2162bf72ca5098fcd3556
Cyrill Zadra <cyrill.zadra@tie.ch>
2025-09-14 16:01:45 +0200

    update

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.handler.logging.LogLevel;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @RestController
 public class DemoController {
@@ -15,13 +20,14 @@ public class DemoController {
 
   @GetMapping
   public String get() {
-    getWebClient().get().uri("https://httpbin.org/get").retrieve()
-        .bodyToMono(String.class).contextCapture().block();
+    getWebClient().get().uri("https://httpbin.org/get").retrieve().bodyToMono(String.class).contextCapture().block();
     return "Hello, World!";
   }
 
   private WebClient getWebClient() {
-    return webClientBuilder.build();
+    HttpClient httpClient = HttpClient.create()
+        .wiretap(WebClient.class.getCanonicalName(), LogLevel.TRACE, AdvancedByteBufFormat.TEXTUAL);
+    return webClientBuilder.clientConnector(new ReactorClientHttpConnector(httpClient)).build();
   }
 
 }
