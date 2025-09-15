33569dc5b0029dcbf6d8ba9d2f507722b99fce20
Cyrill Zadra <cyrill.zadra@tie.ch>
2025-09-15 13:15:32 +0200

    remove unsued import in controller

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import io.netty.handler.logging.LogLevel;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @RestController
 public class DemoController {
