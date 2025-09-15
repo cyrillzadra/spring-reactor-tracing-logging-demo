6cbdf6be6d0422384fa284dacad19116dcb70fb3
Cyrill Zadra <cyrill.zadra@tie.ch>
2025-09-15 13:20:06 +0200

    remove Hook , CorrelationId test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,22 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.micrometer.context.ContextRegistry;
-
 @SpringBootApplication
 public class DemoApplication {
 
-  private static ThreadLocal<String> CORRELATION_ID = ThreadLocal.withInitial(() -> "");
-
-	public static void main(String[] args) {
-
-    ContextRegistry.getInstance()
-        .registerThreadLocalAccessor("traceId",
-            CORRELATION_ID::get,
-            CORRELATION_ID::set,
-            CORRELATION_ID::remove);
-
+  public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
-	}
+  }
 
 }
--- src/main/java/com/example/demo/DemoConfiguration.java
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -2,16 +2,8 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.annotation.PostConstruct;
-import reactor.core.publisher.Hooks;
-
 @Configuration
 public class DemoConfiguration {
-
-
-  @PostConstruct
-  public void init() {
-    Hooks.enableAutomaticContextPropagation();
-  }
+  
 
 }
